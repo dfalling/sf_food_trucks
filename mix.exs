80c55950197b03d1c526bf7b9a9dc883a1ac306b
Dennis Fallin <dfalling@gmail.com>
2023-03-30 14:54:16 +0800

    Move lint steps to single mix lint command

--- mix.exs
+++ mix.exs
@@ -67,6 +67,13 @@ defmodule FoodTrucks.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      lint: [
+        "compile --warnings-as-errors --force",
+        "format --check-formatted --dry-run",
+        "credo --strict",
+        "hex.audit",
+        "deps.audit"
+      ],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
